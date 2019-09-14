b575d062d6b3b9d5eb7b2a58e79c8fade1edc81c
Jens den Braber <jens.denbraber@gmail.com>
2019-09-14 14:24:14 +0200

    Create MMM-DeGiro.js

---
+++ MMM-DeGiro.js
@@ -0,0 +1,14 @@
+/* Magic Mirror
+ * Module: DeGiro
+ *
+ * By Prog Party! https://github.com/Prog-Party
+ * Jens (Epic) den Braber & Dennis (Awesome) Rosenbaum
+ * MIT Licensed.
+ */
+ 
+ Module.register("MMM-DeGiro" ,{
+	// Define module defaults
+	defaults: {
+	}
+ });
+ 
